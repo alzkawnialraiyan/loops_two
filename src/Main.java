d2a53a604a78dc0fd243f56992f4cb2151600cc0
al raiyan al zakwani <alzakwanialraiyan0@gmail.com>
2023-08-18 13:07:51 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //while loop start at 10 , check condition , print first then decrement or increment.
+        int x = 10;
+        while (x >= 1){
+            System.out.println(x);
+            x--;
+        }
+    }
+}
